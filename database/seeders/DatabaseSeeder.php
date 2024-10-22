c1084baa144e7fb8e46f9181fd429ee854a17c48
Chema <chchema26@gmail.com>
2024-10-22 11:38:43 -0600

    Preparativos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Post;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -18,5 +20,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Post::factory(5)->create();
     }
 }
